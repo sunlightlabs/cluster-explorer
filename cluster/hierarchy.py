e3495f88ac2a5c6c57c143aa13c6116981fd897f
Ethan Phelps-Goodman <ethan@sunlightfoundation.com>
2011-07-12 12:31:34 -0400

    dumping and loading of stepwise clustering

--- cluster/hierarchy.py
+++ cluster/hierarchy.py
@@ -1,34 +1,41 @@
 
+from cftc import CFTCDocument
+from interactive import dump_to_csv
 import cPickle
 import csv
 import sys
 
-from interactive import dump_to_csv
-from cftc import CFTCDocument
 
+class IndirectList(object):
+    
+    def __init__(self, indexes, values):
+        self.indexes = indexes
+        self.values = values
+        
+    def __len__(self):
+        return len(self.indexes)
+    
+    def __getitem__(self, i):
+        return self.values[self.indexes[i]]
+    
+    def __iter__(self):
+        for i in xrange(len(self)):
+            yield self[i]
 
 
 def load_hierarchy(path):
-    return [[], 
-            [["A sample doc", "Another sample doc"]], 
-            [["A sample doc", "Another sample doc"], ["Some unrelated stuff.", "More unrelated stuff.", "And a third unrelated thing."]]]
-
-
-
-def singleton_count(cluster_map):
-    c = 0
-    for value in cluster_map.values:
-        if len(value) == 1:
-            c += 1
+    (clustering, docs, steps) = cPickle.load(open(path, 'rb'))
     
-    return c
-
+    return [[IndirectList(cluster, docs) for cluster in clustering] for clustering in steps]
     
+
 if __name__ == '__main__':
     in_file = sys.argv[1]
     out_file = sys.argv[2]
     print "Reading existing clustering from %s..." % in_file 
     (clustering, docs) = cPickle.load(open(in_file, 'rb'))
+    
+    clusterings = list()
     
     i = 0
     while True:
@@ -38,9 +45,15 @@ if __name__ == '__main__':
         
         clustering.merge(x, y)
         
-        dump_to_csv(clustering, docs, "%s.%d.csv" % (out_file, i))
+        clusters = [c for c in clustering.get_clusters().values() if len(c) > 1]
+        clusters.sort(key=len, reverse=True)        
         
+        clusterings.append(clusters)
+        #dump_to_csv(clustering, docs, "%s.%d.csv" % (out_file, i))
+        
+        sys.stdout.write('.')
         i += 1
-        
-        
-        
+    
+    print "\n Writing result of %d steps..." % i    
+    cPickle.dump((clustering, docs, clusterings), open(out_file, 'wb'), cPickle.HIGHEST_PROTOCOL)
+    
