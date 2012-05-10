728d50a4986d5894fbae447ce293e56e792d50bf
Ethan Phelps-Goodman <ethan@sunlightfoundation.com>
2012-05-09 17:06:45 -0700

    Removed finicky pyes, use urllib2 instead.

--- analysis/management/commands/load_elasticsearch.py
+++ analysis/management/commands/load_elasticsearch.py
@@ -1,25 +1,24 @@
 
 from datetime import datetime
 from optparse import make_option
+import json
+import urllib2
 
 from django.db import transaction
 from django.core.management.base import BaseCommand
-from pyes import ES, TermQuery
 
 from analysis.corpus import Corpus
 from analysis.ingestion import DocumentIngester
 from analysis.parser import ngram_parser
 
 
-def load_docket(c, docket):
+def load_docket(es_endpoint, docket):
     docs = list()
     
-    try:
-        results = c.search(TermQuery("docket_id", docket), size=1000000)
-    except pyes.urllib3.connectionpool.TimeoutError:
-        # had problems with timeouts, so give a second try before giving up
-        results = c.search(TermQuery("docket_id", docket), size=1000000)
-    
+    query = {'size':1000000, 'filter': { 'term': { 'docket_id': docket } } }
+    request = urllib2.urlopen(es_endpoint, json.dumps(query))
+    results = json.load(request)
+        
     for r in results['hits']['hits']:
         text = "\n".join([file['text'].encode('ascii', 'replace') for file in r['_source']['files'] if len(file['text']) > 0])
         metadata = dict([(key, str(value)) for (key, value) in r['_source'].items() if key != 'files' and value is not None])
@@ -40,12 +39,11 @@ def ingest_docket(agency, docket, docs, ngrams=None):
     print "Finished processing at %s" % datetime.now()
     print "Added %d documents in corpus %d" % (len(docs), c.id)
 
-def get_dockets(c, agency):
+def get_dockets(es_endpoint, agency):
 
     query = {
         "from": 0,
         "size": 0,
-        "query": { "match_all" : {} },
         "facets": {
             "docket_id": {
                 "terms": { "field": "docket_id", "size": 1000000 },
@@ -53,8 +51,10 @@ def get_dockets(c, agency):
             }
         }
     }
+    request = urllib2.urlopen(es_endpoint, json.dumps(query))
+    results = json.load(request)
     
-    return [r['term'] for r in c.search(query)['facets']['docket_id']['terms'] if r['count'] >= 50]
+    return [r['term'] for r in results['facets']['docket_id']['terms'] if r['count'] >= 50]
 
 
 class Command(BaseCommand):
@@ -62,25 +62,20 @@ class Command(BaseCommand):
         make_option("-n", "--ngrams", dest="ngrams"),
         make_option('-a', "--agency", dest="agency"),
         make_option('-d', "--docket", dest="docket"),
-        make_option('-s', "--es_server", dest="server"),
+        make_option('-s', "--es_server", dest="server", default="http://localhost:9200/_search"),
     )
 
     @transaction.commit_on_success
     def handle(self, **options):
         print "Loading data from ElasticSearch at %s" % datetime.now()
         
-        if options.get('server'):
-            c = ES(options['server'])
-        else:
-            c = ES()
-        
         if options.get('docket'):
             dockets = [options['docket']]
         else:
-            dockets = get_dockets(c, options['agency'])
+            dockets = get_dockets(options['server'], options['agency'])
             
         for docket in dockets:
-            docs = load_docket(c, docket)
+            docs = load_docket(options['server'], docket)
             ingest_docket(options['agency'], docket, docs, options.get('ngrams'))
 
         print "Done."
