5adabc185617c665a2f426baed4dedd5b4c238eb
Ethan Phelps-Goodman <ethan@sunlightfoundation.com>
2012-05-16 16:24:54 -0700

    Trying new, hopefully faster Partition implementation.

--- analysis/partition.py
+++ analysis/partition.py
@@ -5,20 +5,25 @@ from collections import defaultdict
 class Partition(object):
     
     def __init__(self, values):
-        self.map = dict([(x, x) for x in values])
+        # these two don't change after initialization
+        self.values = list(values)
+        self.value_positions = dict(zip(values, range(len(values))))
+        
+        # this map is updated by merge()
+        self.map = range(len(values))
         
     def merge(self, x, y):
-        x_representative = self.map[x]
-        y_representative = self.map[y]
+        x_representative = self.map[self.value_positions[x]]
+        y_representative = self.map[self.value_positions[y]]
 
         if x_representative != y_representative:
-            for (key, value) in self.map.items():
-                if value == y_representative:
-                    self.map[key] = x_representative
+            for i in xrange(len(self.map)):
+                if self.map[i] == y_representative:
+                    self.map[i] = x_representative
     
     def sets(self):
         sets = defaultdict(list)
-        for (key, value) in self.map.items():
-            sets[value].append(key)
+        for i in xrange(len(self.map)):
+            sets[self.map[i]].append(self.values[i])
             
         return sets.values()
