4641308cea9be6a38610aee6d302d0d2876f4ca3
Ethan Phelps-Goodman <ethan@sunlightfoundation.com>
2012-08-29 14:03:06 -0700

    Beginning work on streaming similarity read/write.
    Similarity reader/writer classes are just stubs.
    BufferedCompressedReader/Writer appear to work but
    aren't yet thuroughly tested.

--- analysis/bsims.py
+++ analysis/bsims.py
@@ -27,6 +27,127 @@ from utils import profile
 DATA_DIR = getattr(settings, 'SIMS_DATA_DIR', '.')
 
 
+
+STORED_SIMILARITY_CUTOFFS = [0.9, 0.8, 0.7, 0.6, 0.5]
+SIMILARITY_IO_BUFFER_SIZE = 100 * 1024 * 1024 # 100MB, enough so vast majority of corpora fit in single buffer
+
+class SimilarityWriter(object):
+
+	def __init__(self, corpus_id):
+		self.buffers
+
+	def __enter__(self):
+		return self
+
+	def __exit__(self):
+		self.close()
+
+	def write(x, y, s):
+		pass
+		# split on similarity
+		# write to in-meory buffer
+		# when buffer is full, push to compressed temp file
+
+	def close():
+		pass
+		# flush last buffer
+		# move old file to backup, rename temp file, delete backup
+
+
+class SimilarityReader(object):
+	
+
+	def __init__(self):
+		pass
+
+	def __iter__(self):
+		pass
+		# chain iterators on each bucket
+
+	def bucket(cutoff):
+		pass
+		# open compressed file
+
+		# feed in chunks of compressed file to decompressor
+
+		# yield one at a time
+
+
+class BufferedCompressedWriter(object):
+
+	def __init__(self, outstream, buffer_size):
+		self.outputstream = outstream
+		self.compressor = zlib.compressobj()
+		self.buffer_size = buffer_size
+		self.buffer = StringIO()
+
+	def __enter__(self):
+		return self
+
+	def __exit__(self):
+		self.close()
+
+	def write(self, bytes):
+		self.buffer.write(bytes)
+
+		if self.buffer.tell() >= self.buffer_size:
+			self.flush()
+
+	def flush(self):
+		buffered_bytes = self.buffer.getvalue()
+		self.buffer.truncate(0)
+
+		compressed_bytes = self.compressor.compress(buffered_bytes)
+
+		print "writing %s uncompressed bytes as %s compressed bytes." % (len(buffered_bytes), len(compressed_bytes))
+
+		self.outputstream.write(compressed_bytes)
+
+
+	def close(self):
+		self.flush()
+		remaining_compressed_bytes = self.compressor.flush()
+		self.outputstream.write(remaining_compressed_bytes)
+		print "wrote %s remaining compressed bytes." % len(remaining_compressed_bytes)
+		self.outputstream.flush()
+		self.compressor = None
+
+
+class BufferedCompressedReader(object):
+
+	def __init__(self, inputstream, buffer_size):
+		self.inputstream = inputstream
+		self.decompressor = zlib.decompressobj()
+		self.compressed_buffer = StringIO()
+		self.decompressed_buffer = StringIO()
+		self.buffer_size = buffer_size
+
+	def read(self, byte_count=None):
+		eof = False
+		while not eof and self.decompressed_buffer.tell() < byte_count:
+			print "have %s uncompressed bytes, need %s more..." % (self.decompressed_buffer.tell(), byte_count - self.decompressed_buffer.tell())
+			# make sure the number of compressed bytes is at least as large as then number of decompressed bytes we still need
+			while not eof and len(self.decompressor.unconsumed_tail) + self.compressed_buffer.tell() < byte_count - self.decompressed_buffer.tell():
+				new_compressed_bytes = self.inputstream.read(self.buffer_size)
+				print "have %s unconsumed and %s buffered compressed bytes, read %s more." % (len(self.decompressor.unconsumed_tail), self.compressed_buffer.tell(), len(new_compressed_bytes))
+				if not new_compressed_bytes:
+					eof = True
+				else:
+					self.compressed_buffer.write(new_compressed_bytes)
+
+			compressed_bytes = self.decompressor.unconsumed_tail + self.compressed_buffer.getvalue()
+			self.compressed_buffer.truncate(0)
+			new_uncompressed_bytes = self.decompressor.decompress(compressed_bytes, byte_count - self.decompressed_buffer.tell())
+			self.decompressed_buffer.write(new_uncompressed_bytes)
+			print "turned %s compressed bytes into %s decompressed bytes and %s remaining compressed bytes." % (len(compressed_bytes), len(new_uncompressed_bytes), len(self.decompressor.unconsumed_tail))
+
+		decompressed = self.decompressed_buffer.getvalue()
+		self.decompressed_buffer.close()
+		self.decompressed_buffer = StringIO()
+		self.decompressed_buffer.write(decompressed[byte_count:])
+		return decompressed[:byte_count]
+
+
 def migrate_similarities():
 	cursor = connection.cursor()
 
