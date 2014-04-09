bd6c7a879c765184f5ed7b4a052de53ce890fedf
Andrew Pendleton <apendleton@sunlightfoundation.com>
2014-04-09 11:28:45 -0400

    Stop trying to process unchanged dockets all the time for no reason.

--- analysis/management/commands/analyze_regs.py
+++ analysis/management/commands/analyze_regs.py
@@ -10,6 +10,7 @@ from analysis.parser import ngram_parser, sentence_parse
 from analysis import bsims
 
 from regs_models import Docket, Doc
+from mongoengine.queryset import Q
 
 def doc_text(doc):
     return "\n".join([doc.canonical_view().as_text()] + [a.canonical_view().as_text() for a in doc.attachments])[:10000]
@@ -43,14 +44,14 @@ def print_stats(docket_id):
             print "Corpus %s (%s) has %s documents." % (corpus.id, corpus.metadata, corpus.num_docs())
 
 
-def ingest_docket(docket):
-    print "Loading docket %s at %s..." % (docket.id, datetime.now())
+def ingest_docket(docket_id):
+    print "Loading docket %s at %s..." % (docket_id, datetime.now())
 
-    deletions = list(Doc.objects(docket_id=docket.id, deleted=True, in_cluster_db=True, type='public_submission').scalar('id'))
+    deletions = list(Doc.objects(docket_id=docket_id, deleted=True, in_cluster_db=True, type='public_submission').scalar('id'))
 
     insertions = [
         dict(text=doc_text(d), metadata=doc_metadata(d))
-        for d in Doc.objects(docket_id=docket.id, deleted=False, in_cluster_db=False, type='public_submission')]
+        for d in Doc.objects(docket_id=docket_id, deleted=False, in_cluster_db=False, type='public_submission')]
 
     
     print "Found %s documents for deletion, %s documents for insertion or update." % (len(deletions), len(insertions))
@@ -59,8 +60,8 @@ def ingest_docket(docket):
         return
 
     with transaction.commit_on_success():
-        ingest_single_parse(docket, deletions, insertions, 'sentence')
-        ingest_single_parse(docket, deletions, insertions, '4-gram')
+        ingest_single_parse(docket_id, deletions, insertions, 'sentence')
+        ingest_single_parse(docket_id, deletions, insertions, '4-gram')
 
     print "Marking MongoDB documents as analyzed at %s..." % datetime.now()
     update_count = Doc.objects(id__in=[d['metadata']['document_id'] for d in insertions]) \
@@ -73,16 +74,18 @@ def ingest_docket(docket):
         print "ERROR: %s documents deleted in Postgres, but only %s documents marked as deleted in MongoDB." % (len(deletions), update_count)
 
 
-def ingest_single_parse(docket, deletions, insertions, parser):
+def ingest_single_parse(docket_id, deletions, insertions, parser):
     if parser not in ('sentence', '4-gram'):
         raise "Parser must be one of 'sentence' or '4-gram'. Got '%s'." % parser
 
-    corpora = get_corpora_by_metadata('docket_id', docket.id)
+    corpora = get_corpora_by_metadata('docket_id', docket_id)
 
     parsed_corpora = [c for c in corpora if c.metadata.get('parser') == parser]
 
     if len(parsed_corpora) == 0:
-        c = Corpus(metadata=dict(docket_id=docket.id, agency_id=docket.agency, parser=parser))
+        dockets = list(Docket.objects(id=docket_id).only('agency'))
+        docket = dockets[0] if dockets else Docket()
+        c = Corpus(metadata=dict(docket_id=docket_id, agency_id=docket.agency if docket.agency else docket_id.split("-")[0], parser=parser))
         print "Created new corpus #%s for %s parse." % (c.id, parser)
     
     elif len(parsed_corpora) == 1:
@@ -93,7 +96,7 @@ def ingest_single_parse(docket, deletions, insertions, parser):
         c.delete_by_metadata('document_id', deletions + [d['metadata']['document_id'] for d in insertions])
     
     else:
-        raise "More than one sentence parse for docket %s found. Shouldn't happen--will need ot manually remove extra corpora." % docket.id
+        raise "More than one sentence parse for docket %s found. Shouldn't happen--will need ot manually remove extra corpora." % docket_id
     
     print "Inserting documents at %s..." % datetime.now()
     if parser == 'sentence':
@@ -106,7 +109,7 @@ def ingest_single_parse(docket, deletions, insertions, parser):
     c.delete_hierarchy_cache()
 
 
-def repair_missing_docket(docket):
+def repair_missing_docket(docket_id):
     """Recreate any dockets that Mongo thinks are analyzed already but aren't in Postgres.
 
     Note that this is a very limited form or repair, corresponding to the particular
@@ -116,55 +119,55 @@ def repair_missing_docket(docket):
     """
 
     # only repair if MongoDB thinks that something should be in Postgres already
-    if Doc.objects(docket_id=docket.id, in_cluster_db=True).count() == 0:
+    if Doc.objects(docket_id=docket_id, in_cluster_db=True).count() == 0:
         return
 
     # does docket exist at all?
-    corpora = get_corpora_by_metadata('docket_id', docket.id)
+    corpora = get_corpora_by_metadata('docket_id', docket_id)
     
     if len(corpora) == 0:
         # neither parse exists, mark as unclustered in Mongo
-        update_count = Doc.objects(docket_id=docket.id, in_cluster_db=True).update(safe_update=True, set__in_cluster_db=False)
-        print "Docket %s missing in Postgres. Marked %s documents with in_cluster_db=False." % (docket.id, update_count)
-        ingest_docket(docket)
+        update_count = Doc.objects(docket_id=docket_id, in_cluster_db=True).update(safe_update=True, set__in_cluster_db=False)
+        print "Docket %s missing in Postgres. Marked %s documents with in_cluster_db=False." % (docket_id, update_count)
+        ingest_docket(docket_id)
     elif len(corpora) == 1 or len(corpora) > 2:
         # we have a single or multiple parses...that's something unexpected that we can't fix automatically
-        raise "Found %s corpora for docket %s. Expected either 0 or 2 corpora. Must fix by hand." % (len(corpora), docket.id)
+        raise "Found %s corpora for docket %s. Expected either 0 or 2 corpora. Must fix by hand." % (len(corpora), docket_id)
 
     # both parses exist, everything's fine
 
-def repair_missing_sims(docket):
+def repair_missing_sims(docket_id):
     """Repair the situation where a docket is correct in Mongo and Postgres,
     but the similarity directory is missing."""
 
     with transaction.commit_on_success():
-        c = get_dual_corpora_by_metadata('docket_id', docket.id)
+        c = get_dual_corpora_by_metadata('docket_id', docket_id)
         if c and not bsims.exists(c.id):
-                print "Docket %s (id=%s) missing similarities. Starting recomputation at %s..." % (docket.id, c.id, datetime.now())
+                print "Docket %s (id=%s) missing similarities. Starting recomputation at %s..." % (docket_id, c.id, datetime.now())
                 i = DocumentIngester(c)
                 i.compute_similarities()
 
 
-def delete_analysis(docket):
+def delete_analysis(docket_id):
     with transaction.commit_on_success():
-        c = get_dual_corpora_by_metadata('docket_id', docket.id)
+        c = get_dual_corpora_by_metadata('docket_id', docket_id)
         if c:
             c.delete_corpus()
-            print "Deleted docket %s (id=%s)." % (docket.id, c.id)
+            print "Deleted docket %s (id=%s)." % (docket_id, c.id)
         else:
-            print "Attempted deletion of %s. Docket not found." % docket.id
-        Doc.objects(docket_id=docket.id).update(set__in_cluster_db=False)
+            print "Attempted deletion of %s. Docket not found." % docket_id
+        Doc.objects(docket_id=docket_id).update(set__in_cluster_db=False)
 
-def process_docket(docket, options):
+def process_docket(docket_id, options):
     with transaction.commit_manually():
         if options.get('repair'):
-            repair_missing_docket(docket)
+            repair_missing_docket(docket_id)
         elif options.get('delete'):
-            delete_analysis(docket)
+            delete_analysis(docket_id)
         elif options.get('repair_sims'):
-            repair_missing_sims(docket)
+            repair_missing_sims(docket_id)
         else:
-            ingest_docket(docket)
+            ingest_docket(docket_id)
 
 class Command(BaseCommand):
     option_list = BaseCommand.option_list + (
@@ -177,15 +180,14 @@ class Command(BaseCommand):
     )
 
     def handle(self, **options):        
+        doc_kwargs = {'type': 'public_submission'}
         if options.get('docket'):
-            dockets = Docket.objects(id=options['docket'])
+            doc_kwargs['docket_id'] = options['docket']
         elif options.get('agency'):
-            dockets = Docket.objects(agency=options['agency'])
-        else:
-        	dockets = Docket.objects()
+            doc_kwargs['agency'] = options['agency']
 
         print "Enumerating dockets..."
-        docket_list = list(dockets.only('id', 'agency'))
+        docket_list = list(Doc.objects(Q(deleted=True, in_cluster_db=True) | Q(deleted=False, in_cluster_db=False), **doc_kwargs).distinct('docket_id'))
         docket_count = len(docket_list)
         counter = 0
 
@@ -194,20 +196,20 @@ class Command(BaseCommand):
         if options['fork']:
             print "Using forking strategy..."
             import multiprocessing
-            for docket in docket_list:
+            for docket_id in docket_list:
                 counter += 1
                 print "Docket #%s / %s" % (counter, docket_count)
 
-                p = multiprocessing.Process(target=process_docket, args=[docket, options])
+                p = multiprocessing.Process(target=process_docket, args=[docket_id, options])
                 p.start()
                 p.join()
         else:
             print "Using single-process strategy..."
-            for docket in docket_list:
+            for docket_id in docket_list:
                 counter += 1
                 print "Docket #%s / %s" % (counter, docket_count)
 
-                process_docket(docket, options)
+                process_docket(docket_id, options)
 
         print "Done."
 
